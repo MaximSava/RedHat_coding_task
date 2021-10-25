dd93cbccdf3ddc404e8a9b391fbecfc0b23f15ef
Max S <maximbt@gmail.com>
2021-10-25 12:27:14 +0300

    Pep errors fixing

--- Lesson_4/lesson_4.py
+++ Lesson_4/lesson_4.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 
-chrome_driver = webdriver.Chrome(executable_path ="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
-gecko_driver = webdriver.Firefox(executable_path ="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\geckodriver-v0.30.0-win64\\geckodriver.exe")
+chrome_driver = webdriver.Chrome(executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4"
+                                                 "\\chromedriver_win32\\chromedriver.exe")
+gecko_driver = webdriver.Firefox(executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4"
+                                                 "\\geckodriver-v0.30.0-win64\\geckodriver.exe")
 
 # 1
 
@@ -12,7 +14,7 @@ gecko_driver.get('http:/ynet.co.il')
 # Walla site title compare
 title_walla = 'וואלה! - האתר המוביל בישראל - עדכונים מסביב לשעון'
 if chrome_driver.title == title_walla:
-    print('Walla title matched'
+    print('Walla title matched')
 else:
     print('Title not matched')
 
@@ -24,12 +26,12 @@ else:
     print('Title not matched')
 
 # 3
-from selenium import webdriver
 
 chrome_driver = webdriver.Chrome(
-executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
+    executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
 gecko_driver = webdriver.Firefox(
-executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\geckodriver-v0.30.0-win64\\geckodriver.exe")
+    executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\geckodriver-v0.30.0-win64"
+                    "\\geckodriver.exe")
 
 gecko_driver.get('http://ynet.co.il')
 chrome_driver.get('http://ynet.co.il')
@@ -41,35 +43,34 @@ gecko_driver.find_element_by_xpath('//*[@id="headerWeatherSelect"]/a').click()
 # 4
 
 chrome_driver.get('https://translate.google.com')
-chrome_driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea').send_keys("שלום")
+chrome_driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div['
+                                    '2]/c-wiz[1]/span/span/div/textarea').send_keys("שלום")
 
 # 5
-from selenium import webdriver
 
 chrome_driver = webdriver.Chrome(
-executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
+    executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
 chrome_driver.implicitly_wait(10)
 
 chrome_driver.get('https://youtube.com')
-chrome_driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div/div[1]/input').send_keys('queen')
+chrome_driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div['
+                                    '2]/ytd-searchbox/form/div/div[1]/input').send_keys('queen')
 chrome_driver.find_element_by_xpath('//*[@id="search-icon-legacy"]').click()
 
 # 6
-from selenium import webdriver
 chrome_driver = webdriver.Chrome(
-executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
+    executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
 chrome_driver.implicitly_wait(10)
 
 chrome_driver.get('https://translate.google.com')
-chrome_driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]' \
+chrome_driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]'
                                     '/div[2]/c-wiz[1]/span/span/div/textarea').send_keys("שלום")
-chrome_driver.find_element_by_class_name("er8xn").send_keys('test').
+chrome_driver.find_element_by_class_name("er8xn").send_keys('test')
 chrome_driver.find_element_by_name("D5aOJc vJwDU").send_keys('test')
 
 # 7
-from selenium import webdriver
 chrome_driver = webdriver.Chrome(
-executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
+    executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
 chrome_driver.implicitly_wait(10)
 
 chrome_driver.get('http://facebook.com')
@@ -80,25 +81,22 @@ chrome_driver.find_element_by_class_name('_6ltg').click()
 chrome_driver.find_element_by_xpath('//*[@id="email"]').get_attribute('email')
 
 # 8
-from selenium import webdriver
 chrome_driver = webdriver.Chrome(
-executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
+    executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
 chrome_driver.implicitly_wait(10)
 chrome_driver.get('https://youtube.com')
 
 # Print all cookies
 print(chrome_driver.get_cookies())
 
-#delete all Cookies
+# delete all Cookies
 chrome_driver.delete_all_cookies()
 print(chrome_driver.get_cookies())
 
 # 10
 
-from selenium import webdriver
-
 chrome_driver = webdriver.Chrome(
-executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
+    executable_path="C:\\Users\\MaxSa\\PycharmProjects\\DevOps-Course\\Lesson_4\\chromedriver_win32\\chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-extensions")
 chrome_driver.implicitly_wait(10)

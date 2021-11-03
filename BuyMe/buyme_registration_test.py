8a4f0b5b0a3531710bcb790ef18b4cf53d8a5dea
MaxSa <maximbt@gmail.com>
2021-11-03 21:12:56 +0200

    Buy me site sanity test

--- BuyMe/buyme_registration_test.py
+++ BuyMe/buyme_registration_test.py
@@ -2,15 +2,7 @@
 Buyme registration screen test
 """
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import buyme_test
-from selenium.webdriver.chrome.options import Options
-
 
 # Registration
 registration_list = {
--- BuyMe/buyme_test.py
+++ BuyMe/buyme_test.py
@@ -23,7 +23,7 @@ chrome_options = Options()
 # chrome_options.add_argument("--disable-extensions")
 # chrome_options.add_argument("--disable-gpu")
 # chrome_options.add_argument("--no-sandbox") # linux only
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 # chrome_options.headless = True # also works
 chrome_driver = webdriver.Chrome(executable_path=CHROME_WEBDRIVER_PATH, options=chrome_options)
 
